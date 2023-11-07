5e1ee37ce00309205c74a423b27919c3faaf5f38
Denimwearer <CorderoDixon12@yahoo.com>
2023-11-07 13:28:07 -0500

    we learned a lot about variables and logic

--- intro.js
+++ intro.js
@@ -1,1 +1,31 @@
-"Hello";
+console.log("Hello :)");
+
+const firstName = "Cordero";
+let age = 32;
+let isCool = true;
+let capStoneName = null;
+let newTattoo = undefined;
+
+console.log(firstName);
+console.log(
+  `Hello ${firstName}, who has been on earth strong for ${age} years`
+);
+
+let x = 40;
+let y = 10;
+let z = 7 + 8;
+
+let sum = x - y * z;
+console.log(sum);
+
+if (x > y) {
+  console.log("X is greater than Y!");
+} else {
+  console.log("Y is greater than X!");
+}
+
+if (firstName === "Joely") {
+  console.log("I'm the GOAT!");
+} else {
+  console.log("You're just a pretender!");
+}
